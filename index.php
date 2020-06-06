d43d688b42ddce0aa753db7386a94ec11e5300b0
JakubVavru <60426585+JakubVavru@users.noreply.github.com>
2020-06-06 18:48:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
 <?php
 $submit = filter_input(INPUT_POST, 'submit');
 $dough = filter_input(INPUT_POST, 'dough');
-$ham = filter_input(INPUT_POST, 'ham');
-$fefe = filter_input(INPUT_POST, 'fefe');
-$fich = filter_input(INPUT_POST, 'fish');
-$cheese = filter_input(INPUT_POST, 'cheese');
-$tomato = filter_input(INPUT_POST, 'tomato');
-$mushrom = filter_input(INPUT_POST, 'mushrom');
-$pine = filter_input(INPUT_POST, 'pine');
-$salami = filter_input(INPUT_POST, 'salami');
-$sea = filter_input(INPUT_POST, 'sea');
-$chick = filter_input(INPUT_POST, 'chick');
-$corn = filter_input(INPUT_POST, 'corn');
+$ingredients = $_POST['ingredients'];
 ?>
 <!DOCTYPE html>
 <html lang="cs">
@@ -27,61 +17,18 @@ $corn = filter_input(INPUT_POST, 'corn');
  
         <h2>Potvrzení objednávky</h2>
         <p>Děkujeme za vaši objednávku, kterou vám odvezeme co nejdříve.</p>
-        <p>Objednal jste si pizzu s:</p>
+        <p>Objednal jste si </p>
         <p><?= $dough ?> těsto</p>
-        <?php 
-        if(isset($cheese)) { ?>
-            <p>se sýrem</p>
-        <?php } 
-        if(isset($ham)) { ?>
-            <p>se šunkou</p>
-        <?php } 
+        <h3>Ingredience</h3>
         
+        <ul>
+        <?php
+        foreach ($ingredients as $item) { ?>
+            <li><?= $item; ?></li>
+        <?php }
+ 
         ?>
-        <?php 
-        if(isset($fefe)) { ?>
-            <p>s feferonkami</p>
-        <?php } 
-
-        if(isset($fich)) { ?>
-            <p>s rybištou</p>
-        <?php } 
-        
-        ?>
-        <?php 
-        if(isset($tomato)) { ?>
-            <p>s rajčetem</p>
-        <?php } 
-        if(isset($mushrom)) { ?>
-            <p>s houbamy</p>
-        <?php } 
-        
-        ?>
-        <?php 
-        if(isset($pine)) { ?>
-            <p>s peklem</p>
-        <?php } 
-
-        if(isset($salami)) { ?>
-            <p>se salámem</p>
-        <?php } ?>
-
-        <?php  
-        if(isset($sea)) { ?>
-            <p>s mořskými plody</p>
-        <?php } 
-        
-        ?>
-        <?php 
-        if(isset($chick)) { ?>
-            <p>s kousky kuřete</p>
-        <?php } 
-
-        if(isset($corn)) { ?>
-            <p>s kukuřicí</p>
-        <?php } 
-        
-        ?>        
+        </ul>       
 
         
 
@@ -90,50 +37,50 @@ $corn = filter_input(INPUT_POST, 'corn');
             
             <form action="index.php" method="post">
         <h2>Typ těsta</h2>
-        <input type="radio" name="dough" id="normal" value="normal">
+        <input type="radio" name="dough" id="normal" value="Normální">
         <label for="normal">Normální</label></br>
         
-        <input type="radio" name="dough" id="glutenfree" value="glutenfree">
+        <input type="radio" name="dough" id="glutenfree" value="Glutenfree">
         <label for="glutenfree">Bezlepkové</label></br>
         
-        <h2>Maso</h2>
+        <h2>Ingredience</h2>
                 
-        <input type="checkbox" name="ham" id="ham">
+        <input type="checkbox" name="ingredients[]" id="ham" value="Šunka">
         <label for="ham">Šunka</label></br>
         
-        <input type="checkbox" name="fefe" id="fefe">
+        <input type="checkbox" name="ingredients[]" id="fefe" value="Feferonky">
         <label for="fefe">Feferonky</label></br>        
         
-        <input type="checkbox" name="fish" id="fish">
+        <input type="checkbox" name="ingredients[]" id="fish" value="Rybišty">
         <label for="fish">Rybišty</label></br>       
         
-        <input type="checkbox" name="cheese" id="cheese">
+        <input type="checkbox" name="ingredients[]" id="cheese" value="Sýr">
         <label for="cheese">Sýr</label></br>
 
-        <input type="checkbox" name="tomato" id="tomato">
+        <input type="checkbox" name="ingredients[]" id="tomato" value="Rajče">
         <label for="tomato">Rajče</label></br>
         
-        <input type="checkbox" name="mushrom" id="mushrom">
+        <input type="checkbox" name="ingredients[]" id="mushrom" value="Houby">
         <label for="mushrom">Houby</label></br>        
         
-        <input type="checkbox" name="pine" id="pine">
+        <input type="checkbox" name="ingredients[]" id="pine" value="Ananas (jako vážně?!)">
         <label for="pine">Ananas (jako vážně?!)</label></br>       
         
-        <input type="checkbox" name="salami" id="salami">
+        <input type="checkbox" name="ingredients[]" id="salami" value="Salám">
         <label for="salami">Salám</label></br>
 
-        <input type="checkbox" name="sea" id="sea">
+        <input type="checkbox" name="ingredients[]" id="sea" value="Mořský plody">
         <label for="sea">Mořský plody</label></br>        
         
-        <input type="checkbox" name="chick" id="chick">
+        <input type="checkbox" name="ingredients[]" id="chick" value="Kuře">
         <label for="chick">Kuře</label></br>       
         
-        <input type="checkbox" name="corn" id="corn">
+        <input type="checkbox" name="ingredients[]" id="corn" value="Kukuřice">
         <label for="corn">Kukuřice</label></br>
 
-        <input type="submit" name="submit" value="Odeslat">
+        <input type="submit" name="submit" value="Odeslat" value="">
     </form>
-      <?php  }    
+      <?php  } 
     ?>
     
 
